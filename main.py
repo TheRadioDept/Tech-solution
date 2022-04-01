020ac5afbb6889a2fd3e9fd704a85200b4712a00
Farkhad <58568149+TheRadioDept@users.noreply.github.com>
2022-04-01 18:08:10 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+import json
+import sys
+from sys import argv
+import json
+
+#access .json file and load it's insides
+with open('countries.json') as f:
+    countries = json.load(f)
+
+#create a list of translation keys from .json file
+official_keys = {}
+for c in countries:
+    official_keys.setdefault(c['name']['official'], {}).update(c['translations'])
+
+#check if key value is empty or more than 1 s
+if len(sys.argv) == 1 or len(sys.argv) > 2:
+    print("Incorrect parameter")
+    sys.exit()
+else:
+    key = argv[1]
+
+#return countries names if CLI pAarameter matches one of the keys
+for official, keys  in official_keys.items():
+    if (key in keys):
+        print(keys[key]["official"])
+    else : 
+        print ("Translation key is not supported")
+
+f.close()
