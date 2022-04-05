af15c2ba4137d258aced28e85283c0e860c4ff51
Farkhad <58568149+TheRadioDept@users.noreply.github.com>
2022-04-05 13:12:22 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
 import json
 import sys
 
-#access .json file and load it's insides
+# access .json file and load it's insides
 with open("countries.json") as f:
     countries = json.load(f)
-    
-#check if key value is empty or more than 1
+
+# check if key value is empty or more than 1
 if len(sys.argv) == 2:
-     key = sys.argv[1]
+    key = sys.argv[1]
 else:
     print("Incorrect parameter.")
     sys.exit()
 
-#create a list of translation keys from .json file
+# create a list of translation keys from .json file
 official_keys = []
 for i in range(len(countries)):
     for j in countries[i]["translations"]:
         if j not in official_keys:
             official_keys.append(j)
 
-#return countries names if CLI parameter matches one of the keys        
+# return countries names if CLI parameter matches one of the keys
 if key not in official_keys:
     print("Key is not supported!")
 else:
